7c08d72807b845113a897fde7fcd15a6d9950ec9
masculinosm <shemputh03@gmail.com>
2025-04-26 11:37:21 +0800

    tryyy

--- components/TabBar.tsx
+++ components/TabBar.tsx
@@ -39,6 +39,7 @@ export default function TabBar() {
     },
   ];
 
+
   return (
       <View style={[styles.container, { backgroundColor: colors.tabBarBackground, borderTopColor: colors.border }]}>
         {tabs.map((tab) => (
